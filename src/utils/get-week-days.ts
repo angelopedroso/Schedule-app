09a3c8d7d7c65353219446694a66fadcc13b72e4
angelopedroso <solonasusonly0@gmail.com>
2023-02-25 21:12:37 -0300

    register: Add third register page; Add weekday formatter

---
+++ src/utils/get-week-days.ts
@@ -0,0 +1,11 @@
+export function getWeekDays() {
+  const formatter = new Intl.DateTimeFormat('pt-BR', {
+    weekday: 'long',
+  })
+
+  return Array.from(Array(7).keys())
+    .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
+    .map((weekDay) => {
+      return weekDay[0].toUpperCase() + weekDay.slice(1)
+    })
+}
