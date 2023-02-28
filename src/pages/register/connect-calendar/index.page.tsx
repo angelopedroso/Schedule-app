5940bc8f1586a581e6925e945226eaf5b7218e5b
angelopedroso <solonasusonly0@gmail.com>
2023-02-28 00:59:31 -0300

    register: update profile page, profile api

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -17,6 +17,10 @@ export default function ConnectCalendar() {
     await signIn('google')
   }
 
+  async function handleNavigateToNextStep() {
+    await router.push('/register/time-intervals')
+  }
+
   return (
     <Container>
       <Header>
@@ -56,7 +60,11 @@ export default function ConnectCalendar() {
           </AuthError>
         )}
 
-        <Button type="button" disabled={!isSignedIn}>
+        <Button
+          onClick={handleNavigateToNextStep}
+          type="button"
+          disabled={!isSignedIn}
+        >
           Próximo passo
           <ArrowRight />
         </Button>
--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -24,6 +24,7 @@ import { getWeekDays } from '@/utils/get-week-days'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { convertTimeStringToMinutes } from '@/utils/convert-time-string-to-minutes'
 import { api } from '@/lib/axios'
+import { useRouter } from 'next/router'
 
 const timeIntervalsFormsSchema = z.object({
   intervals: z
@@ -89,6 +90,8 @@ export default function TimeIntervals() {
     },
   })
 
+  const router = useRouter()
+
   const weekDays = getWeekDays()
 
   const intervals = watch('intervals')
@@ -102,6 +105,8 @@ export default function TimeIntervals() {
     const { intervals } = data as timeIntervalsFormOutput
 
     await api.post('/users/time-intervals', intervals)
+
+    await router.push('/register/update-profile')
   }
 
   return (
