c424d23e56a404ab33bd98403be5cc5a4a035955
angelopedroso <solonasusonly0@gmail.com>
2023-02-28 22:52:07 -0300

    calendar: add real timepicker header

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,4 +1,6 @@
 import { Calendar } from '@/components/Calendar'
+import dayjs from 'dayjs'
+import { useState } from 'react'
 import {
   Container,
   TimePicker,
@@ -8,33 +10,40 @@ import {
 } from './styles'
 
 export function CalendarStep() {
-  const isDateSelected = false
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+  const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar />
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            terça-feira <span>20 de março</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
 
           <TimePickerList>
             <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>14:00h</TimePickerItem>
             <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>16:00h</TimePickerItem>
+            <TimePickerItem>17:00h</TimePickerItem>
             <TimePickerItem>18:00h</TimePickerItem>
-            <TimePickerItem>04:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>23:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
-            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>19:00h</TimePickerItem>
+            <TimePickerItem>20:00h</TimePickerItem>
+            <TimePickerItem>21:00h</TimePickerItem>
           </TimePickerList>
         </TimePicker>
       )}
